4849969dbb921179c8fe6ac19b17dffba8e5a010
said <sidi112233445566778899@gmail.com>
2024-03-03 23:26:53 +0300

    решение задания 3 модуля 2

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,14 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
+export const omit = (object, ...fields) => {
+  const copyOfObj = { ...object };
 
+  for (const key in copyOfObj) {
+    if (fields.includes(key)) {
+      delete copyOfObj[key];
+    }
+  }
+
+  return copyOfObj;
 };
