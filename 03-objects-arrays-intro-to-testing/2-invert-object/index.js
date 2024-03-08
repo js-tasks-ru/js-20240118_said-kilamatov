103d8e637ed77e2fbe932f9a1c0935ab3c04ba86
said <sidi112233445566778899@gmail.com>
2024-03-08 17:27:16 +0300

    решение задания 2 модуля 3

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,15 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (typeof obj === "undefined") {
+    return;
+  }
+  const map = new Map(Object.entries(obj));
+  const newObj = {};
 
+  for (const [key, value] of map) {
+    newObj[value] = key;
+  }
+
+  return newObj;
 }
