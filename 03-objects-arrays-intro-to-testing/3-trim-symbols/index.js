383a0a86da694451c04ab05b3baf490ef3161d93
said <sidi112233445566778899@gmail.com>
2024-03-08 20:57:07 +0300

    решение задания 3 модуля 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (typeof size === "undefined") {
+    return string;
+  }
 
+  const copyOfString = string.split("");
+  const result = [];
+  let count = 0;
+
+  for (let i = 0; i < copyOfString.length; i++) {
+    const letter = copyOfString[i];
+    if (letter === copyOfString[i - 1]) {
+      count++;
+    } else {
+      count = 1;
+    }
+
+    if (count <= size) {
+      result.push(letter);
+    }
+  }
+
+  return result.join("");
 }
