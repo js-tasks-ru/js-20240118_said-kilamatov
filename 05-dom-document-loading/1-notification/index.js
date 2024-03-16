ae4d6c50552280e1e353bc46d2c317029913147f
said <sidi112233445566778899@gmail.com>
2024-03-16 17:40:30 +0300

    решение задания 1 модуля 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+  static notificationMessage = null;
+  element;
 
+  constructor(text, { duration = 1000, type = "error" } = {}) {
+    this.text = text;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  createElement(template) {
+    const element = document.createElement("div");
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  show(container = document.body) {
+    if (NotificationMessage.notificationMessage) {
+      NotificationMessage.notificationMessage.destroy();
+    }
+
+    container.append(this.element);
+
+    NotificationMessage.notificationMessage = this;
+
+    this.timeoutId = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  createTemplate() {
+    return `
+    <div class="notification ${this.type}" style="--value:20s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.text}
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  remove() {
+    this.element.remove();
+    NotificationMessage.notificationMessage = null;
+  }
+
+  destroy() {
+    this.remove();
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+  }
 }
